24f0c42008dff30b0bc8920cf96efde523301d69
Trevor Sears <trevorsears.main@gmail.com>
2019-06-24 22:03:55 -0400

    Additional reminder from init script added.

--- init.sh
+++ init.sh
@@ -6,8 +6,11 @@ git rm --cached ./ts/tslint.json ./.gitignore
 # Delete the readmes that were used to track otherwise-empty dirs.
 rm ./.d.ts/readme.md ./js/readme.md ./ts/readme.md ./ts/tests/readme.md
 
-# Remind the user to update relevant package information from the template.
+# Remind the user to update relevant package information from the template and commit the above changes.
 echo "Remember to modify the package.json, license.md, and readme.md to correct the relevant package information."
+echo "Also remember to commit the changes that this script has made."
+
+echo "All done, bye!"
 
 # Make the script remove itself.
 rm -- "$0"
