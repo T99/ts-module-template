e83ad3e16930eb8f12f8005fdf6435954e509164
Trevor Sears <trevorsears.main@gmail.com>
2019-07-19 19:01:54 -0400

    Added template-replacement functionality to the init script.

--- init.sh
+++ init.sh
@@ -1,19 +1,75 @@
 #!/usr/bin/env bash
 
+# Print start message.
+	echo "Running js-module-template initialization script..."
+
+# Prompt for relevant package information to find-and-replace template placeholder strings.
+	read -p "  Package name: "			packageName
+	read -p "  Package description: "	packageDesc
+	read -p "  GitHub org: "			gitHubOrg
+	read -p "  NPM org: "				npmOrg
+	read -p "  readme title: "			readmeTitle
+
+# Print a spacer line.
+	echo ""
+
+# If the GitHub organization name was left blank, assume the package belongs to T99.
+	if [[ -z $gitHubOrg ]]; then
+
+		gitHubOrg="T99"
+
+	fi
+
+# Derive the full package name from the NPM organization and base package name.
+	if [[ -z $npmOrg ]]; then
+
+		fullPackageName="${packageName}"
+
+	else
+
+		fullPackageName=$(echo -n "@${npmOrg}/${packageName}")
+
+	fi
+
+# Modify template files to use correct package information.
+	echo -n "  Modifying template files to use correct package information... "
+
+	files=("ts/main.ts" "gulpfile.js" "license.md" "package.json" "readme.md")
+
+	for file in "${files[*]}"; do
+
+		sed -i "s_<name>_${fullPackageName}_g" ${file} > /dev/null 2>&1
+		sed -i "s/<desc>/$packageDesc/g" ${file} > /dev/null 2>&1
+		sed -i "s/<title>/$readmeTitle/g" ${file} > /dev/null 2>&1
+		sed -i "s/<base_name>/$packageName/g" ${file} > /dev/null 2>&1
+		sed -i "s/<github_org>/$gitHubOrg/g" ${file} > /dev/null 2>&1
+
+	done
+
+	echo "DONE"
+
 # Stop tracking certain files that were pulled from the template.
-git rm --cached ./ts/tslint.json ./.gitignore
+	echo -n "  Stopping Git from tracking certain files from the template... "
+	git rm --cached ./ts/tslint.json ./.gitignore > /dev/null 2>&1
+	echo "DONE"
 
 # Delete the readmes that were used to track otherwise-empty dirs.
-rm ./.d.ts/readme.md ./js/readme.md ./ts/readme.md ./ts/tests/readme.md
+	echo -n "  Removing placeholder readme files... "
+	rm ./.d.ts/readme.md ./js/readme.md ./ts/readme.md ./ts/tests/readme.md > /dev/null 2>&1
+	echo "DONE"
 
 # Install the missing packages from our package.json file.
-npm install
+	echo -n "  Installing packages from package.json... "
+	npm install > /dev/null 2>&1
+	echo "DONE"
 
-# Remind the user to update relevant package information from the template and commit the above changes.
-echo "Remember to modify the package.json, license.md, and readme.md to correct the relevant package information."
-echo "Also remember to commit the changes that this script has made."
-
-echo "All done, bye!"
+# Print an 'exiting' statement.
+	echo ""
+	echo "  Exiting..."
+	echo ""
 
 # Make the script remove itself.
-rm -- "$0"
+	rm -- "$0"
+
+# Exit from the script.
+	exit 0
--- readme.md
+++ readme.md
@@ -1,18 +1,18 @@
-# repo-name
-Paragraph of project description here.
+# <title>
+<desc>
 
-### [Find repo-name on NPM.](https://www.npmjs.com/package/repo-name)
+### [Find <name> on NPM.](https://www.npmjs.com/package/<name>)
 
 ## Installation
 Install from NPM with
 ```
-$ npm install --save repo-name
+$ npm install --save <name>
 ```
 
 ## Basic Usage
 Basic usage instructions here.
 
 ## License
-repo-name is made available under the GNU General Public License v3.
+<name> is made available under the GNU General Public License v3.
 
 Copyright (C) 2019 Trevor Sears
