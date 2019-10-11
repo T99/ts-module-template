d63e7ca188573abe14c12bbc1a9f0700c34c804e
T99 <trevorsears.main@gmail.com>
2019-10-10 22:11:28 -0400

    Switched from scripting initialization in Bash to Python.

--- ts/main.ts
+++ ts/main.ts
@@ -8,8 +8,8 @@
  * NPM main class used for exporting this package's contents.
  *
  * @author Trevor Sears <trevorsears.main@gmail.com>
- * @version v0.1.0
- * @since v0.1.0
+ * @version v<version>
+ * @since v<version>
  */
 
 // export { ClassName } from "./class-location";
