10ea518d3bd360eda57a84b3e81630a342c9f76f
Trevor Sears <trevorsears.main@gmail.com>
2021-09-02 10:44:56 -0400

    Updated email and website.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 /*
- *	Created by Trevor Sears <trevorsears.main@gmail.com>.
- *	8:48 PM -- June 16th, 2019.
- *	Project: <name>
+ * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
+ * 8:48 PM -- June 16th, 2019.
+ * Project: <name>
  */
 
 const gulp = require("gulp");
