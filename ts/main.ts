cad93088632d9c9ff92559cce04c5c0c88cc5b0e
Trevor Sears <trevorsears.main@gmail.com>
2021-09-02 10:45:31 -0400

    Updated email and website in `main.ts`.

--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,5 @@
 /*
- *	Created by Trevor Sears <trevorsears.main@gmail.com>.
+ *	Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
  *	10:53 PM -- June 11th, 2019.
  *	Project: <name>
  */
@@ -7,7 +7,7 @@
 /**
  * NPM main class used for exporting this package's contents.
  *
- * @author Trevor Sears <trevorsears.main@gmail.com>
+ * @author Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/)
  * @version v<version>
  * @since v<version>
  */
